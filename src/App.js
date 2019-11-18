c905704d87eea7ad9e6f094076b85dd294038053
Mark Artishuk <markyshuk@gmail.com>
2019-11-18 10:56:12 -0800

    Added/Created Routes/PrivateRoutes and changed background color for lighter contrast

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          <h1>Test</h1>
+        </Route>
+        <Route exact path="/login">
+          {/* Put login component here */}
+        </Route>
+
+        <Route exact path="/register">
+          {/* Put register component here */}
+        </Route>
+
+        <PrivateRoute path="/dashboard">
+          {/* Dashboard component here. */}
+        </PrivateRoute>
+
+        <PrivateRoute path="/worker-profile/:id">
+          {/*Service worker profile here (Passing in ID of worker through path)  */}
+        </PrivateRoute>
+        <PrivateRoute path="/worker-edit/:id">
+          {/* Component for editing worker profile (Passin in ID of worker through path) */}
+        </PrivateRoute>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+export const isAuthenticated = () => {
+  return localStorage.getItem("authToken") ? true : false;
+};
+
+const PrivateRoute = ({ children, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={props =>
-        localStorage.getItem("authToken") ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
+      render={() => (isAuthenticated() ? children : <Redirect to="/login" />)}
     />
   );
 };
