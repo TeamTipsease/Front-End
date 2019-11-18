dbb3cda4a5cd20deea3af2ecb403dafc4ab3797d
Mark Artishuk <markyshuk@gmail.com>
2019-11-18 10:24:49 -0800

    Created PrivateRoute component

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
-const PrivateRoute = () => {
-  return <div></div>;
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("authToken") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
 };
 
 export default PrivateRoute;
