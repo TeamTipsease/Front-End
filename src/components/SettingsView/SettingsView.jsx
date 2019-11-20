f2513794cf940c2b861959ac703ac0548fb09e25
Mark Artishuk <markyshuk@gmail.com>
2019-11-20 11:16:51 -0800

    Made Profile settings form

--- src/components/SettingsView/SettingsView.jsx
+++ src/components/SettingsView/SettingsView.jsx
@@ -2,23 +2,83 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import { TextField, Button } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2)
+    padding: 25,
+    margin: "25px auto",
+    width: "80%",
+    maxWidth: 650
+  },
+  container: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  button: {
+    marginTop: 20
+  },
+  title: {
+    fontSize: 35,
+    textAlign: "center"
+  },
+  textField: {
+    margin: "10px auto"
+  },
+  nameTextField: {
+    width: "70%"
+  },
+  monthTextField: {
+    width: "27%"
   }
 }));
 const SettingsView = () => {
   const classes = useStyles();
   return (
     <Paper className={classes.root}>
-      <Typography variant="h5" component="h3">
-        This is a sheet of paper.
-      </Typography>
-      <Typography component="p">
-        Paper can be used to build surface or other elements for your
-        application.
-      </Typography>
+      <form>
+        <Typography className={classes.title} variant="h5" component="h3">
+          Profile Settings
+        </Typography>
+        <div className={classes.container}>
+          <TextField
+            className={classes.nameTextField}
+            label="Name"
+            margin="normal"
+            variant="outlined"
+          />
+          <TextField
+            className={classes.monthTextField}
+            label="Months At Job"
+            type="number"
+            margin="normal"
+            variant="outlined"
+          />
+        </div>
+
+        <TextField
+          className={classes.textField}
+          label="Tagline"
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          className={classes.textField}
+          label="Info"
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <Button
+          className={classes.button}
+          fullWidth
+          variant="contained"
+          color="secondary"
+        >
+          Submit
+        </Button>
+      </form>
     </Paper>
   );
 };
